21fe5fc5e23e808aff03d8ba297d05d2783cc0e0
Kim <127684089+kimand027ghnj@users.noreply.github.com>
2024-10-30 12:34:40 +0800

    repeatString exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
-};
+const repeatString = (str, count) => {
+    let result = '';
+    if (count == -1) {
+        return "ERROR"
+    } if (str == '') {
+        return ""
+    }
+    
+    for (let i = 0; i < count; i++) {
+        result += str;
+    }
+    return result;
+}
+repeatString('hey', 3)
+repeatString('hello', 10)
+repeatString('hi', 1)
+repeatString('bye', 0)
+repeatString('goodbye', -1)
 
 // Do not edit below this line
 module.exports = repeatString;
