677e3c934d21fa657a383c28870ff5ed94bd9ba6
Kim <127684089+kimand027ghnj@users.noreply.github.com>
2024-11-04 22:44:42 +0800

    added modified and currently working on reverseStrgin.js file

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedWord = word.reverse
+    return reversedWord
 };
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
